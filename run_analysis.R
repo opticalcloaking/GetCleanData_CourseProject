ec4ee5ef9678d7ad4710c9c078ffee13d46c9076
opticalcloaking <stuart.mayo@gmail.com>
2015-07-26 12:19:11 -0700

    Added project analysis R script

---
+++ run_analysis.R
@@ -0,0 +1,77 @@
+# read in features names
+features <- read.csv("./features.txt", header=FALSE, sep=" ",
+                     col.names=c("num", "feature"))
+features$feature <- as.character(features$feature)
+
+# identify features that are either a mean or a standard deviation
+logicalMeanStd <- (grepl("mean()", features$feature, fixed=TRUE) | 
+                     grepl("std()", features$feature, fixed=TRUE))
+
+# read in X training data
+colsToRead <- (logicalMeanStd * 2 - 1) * 16
+colsNames <- features$feature[logicalMeanStd]
+X_train <- read.fwf("./train/X_train.txt", colsToRead, header=FALSE,
+                    col.names=colsNames, buffersize=5)
+
+# read in Y training data
+Y_train <- read.csv("./train/y_train.txt", header=FALSE, col.names="activity")
+
+# read in subject training data
+subj_train <- read.csv("./train/subject_train.txt", header=FALSE,
+                       col.names="subject")
+
+# combine X and Y and subject training data
+train <- cbind(Y_train, subj_train, X_train)
+
+# read in X testing data
+X_test <- read.fwf("./test/X_test.txt", colsToRead, header=FALSE,
+                   col.names=colsNames, buffersize=5)
+
+# read in Y testing data
+Y_test <- read.csv("./test/y_test.txt", header=FALSE, col.names="activity")
+
+# read in subject testing data
+subj_test <- read.csv("./test/subject_test.txt", header=FALSE,
+                      col.names="subject")
+
+# combine X and Y and subject testing data
+test <- cbind(Y_test, subj_test, X_test)
+
+# combine training and testing data
+df.samsung <- rbind(train, test)
+
+# convert integer activity classifications to factor with descriptive levels
+df.samsung$activity <- as.character(df.samsung$activity)
+activity_labels <- read.csv("./activity_labels.txt", header=FALSE, sep=" ")
+for (activity in 1:6) {
+  df.samsung$activity[which(df.samsung$activity == as.character(activity))] <-
+    as.character(activity_labels[activity,2])
+}
+
+# remove excess ellipses in column names
+names(df.samsung) <- gsub("...", ".", names(df.samsung), fixed=TRUE)
+names(df.samsung) <- gsub("..", ".", names(df.samsung), fixed=TRUE)
+
+# treat subject as a character rather than an integer
+df.samsung$subject <- as.character(df.samsung$subject)
+
+# compute means of variables for the activities
+df.activitymeans <- aggregate(. ~ activity,
+                              data=df.samsung[c(1,
+                                                3:length(names(df.samsung)))],
+                              FUN=mean)
+names(df.activitymeans)[1] <- "mean.for"
+df.activitymeans$mean.for <- as.character(df.activitymeans$mean.for)
+
+# compute means of variables for the subjects
+df.subjmeans <-  aggregate(. ~ subject,
+                           data=df.samsung[2:length(names(df.samsung))],
+                           FUN=mean)
+names(df.subjmeans)[1] <- "mean.for"
+df.subjmeans$mean.for <- paste("Subject ", as.character(df.subjmeans$mean.for))
+
+# stack the means into one data frame
+df.means <- rbind(df.activitymeans, df.subjmeans)
+
+# output the means data frame
+write.table(df.means, file="./tidymeans.txt", row.names=FALSE)
